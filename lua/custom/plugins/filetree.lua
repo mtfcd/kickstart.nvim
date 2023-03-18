aeae7bb625dc77c081e7cb4b7b6d9f102a4c125c
mtfcd <mtfcd_cc@126.com>
2023-03-18 15:57:40 +0800

    nvim-tree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,23 +1,15 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  cmd = "Neotree",
-  version = "*",
+  "nvim-tree/nvim-tree.lua",
+  -- cmd = "NvimTreeToggle",
   dependencies = {
-    "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
   },
   config = function()
-    require('neo-tree').setup {
-      filesystem = {
-        filtered_items = {
-          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
+    require('nvim-tree').setup {
+      tab = {
+        sync = {
+          open = true
+        }
       }
     }
   end,
